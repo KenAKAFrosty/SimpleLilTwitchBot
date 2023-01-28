830b0ca2fafd4a57b88e63832ac56ae454685e01
FrostyyPaws <90424167+KenAKAFrosty@users.noreply.github.com>
2023-01-27 22:16:52 -0500

    testing serving to public

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,22 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
 
-
+const port = process.env.PORT || 3000;
 
 main();
-async function main() { 
-    
+async function main() {
+
     setInterval(async () => {
         console.log('hi!');
     }, 4000);
+
+    const app = express();
+    app.get('/', (req, res) => {
+        res.send('Hello World!');
+    });
+
+    app.listen(port, () => {
+        console.log(`Example app listening at http://localhost:${port}`);
+    });
 }
